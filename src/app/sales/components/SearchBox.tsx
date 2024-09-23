4bfc9818bdc20620d03ec15b54b882fe8d6fe4f4
loadlc2003 <banerjeeankush184@gmail.com>
2024-09-23 15:17:35 +0530

    wip-searh-box-filter-box

---
+++ src/app/sales/components/SearchBox.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { Input } from "@/components/ui/input";
+import { Search } from "lucide-react";
+import React, { FC, useState } from "react";
+
+interface SearchBoxProps {}
+const SearchBox: FC<SearchBoxProps> = () => {
+  const [email, setEmail] = useState("");
+  return (
+    <div className="bg-white m-4 flex w-[80%] items-center relative">
+      <div className="absolute left-3">
+        <Search />
+      </div>
+      <Input
+        type="email"
+        value={email}
+        placeholder="Search a Company"
+        onChange={(e) => setEmail(e.target.value)}
+        className="pl-10 h-10"
+      />
+    </div>
+  );
+};
+
+export default SearchBox;
--- src/app/sales/page.tsx
+++ src/app/sales/page.tsx
@@ -1,30 +1,22 @@
-import React, { FC } from "react";
-import { AddCompanyModal } from "./components/AddCompanyModal";
+"use client";
+import React, { FC, useState } from "react";
+
 import CompanyCard from "./components/CompanyCard";
+
+import FilterBox from "./components/FilterBox";
+import SearchBox from "./components/SearchBox";
+import StatusTabs from "./components/StatusTabs";
 
 interface PageProps {}
 const page: FC<PageProps> = () => {
   return (
     <div>
-      <div className="flex justify-between items-center">
-        <div className="flex gap-3 p-8">
-          <div className="bg-[#202020] rounded-2xl text-white w-[150px] justify-center flex p-3 items-center gap-2">
-            Active
-            <div className="bg-white text-red-500 rounded-2xl  text-xs p-2 flex items-center justify-center">
-              121
-            </div>
-          </div>
-          <div className="bg-white rounded-2xl w-[150px] justify-center flex p-3 items-center gap-2">
-            Archived
-            <div className="bg-[#E84B0133] text-red-500 rounded-2xl  text-xs p-2 flex items-center justify-center">
-              121
-            </div>
-          </div>
-        </div>
-        <div>
-          <AddCompanyModal />
-        </div>
+      <StatusTabs />
+      <div className="flex items-center">
+        <SearchBox />
+        <FilterBox />
       </div>
+
       <div className="flex gap-3 flex-col">
         <CompanyCard />
         <CompanyCard />
