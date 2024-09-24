12a0bbf0f36ff7cac77b8d66cdbc4e3f07989ddc
loadlc2003 <banerjeeankush184@gmail.com>
2024-09-24 15:54:51 +0530

    wip-company-details

--- src/app/admin/[companyId]/company/page.tsx
+++ src/app/admin/[companyId]/company/page.tsx
@@ -1,7 +1,101 @@
+import CustomBadge from "@/components/ui/CustomBadge";
+import { HandCoins, Users } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ArrowDownToLine, EllipsisVertical } from "lucide-react";
 import React from "react";
+import { Separator } from "@/components/ui/separator";
+import JobTable from "../components/JobTable";
+import SearchBox from "../components/SearchBox";
+import FilterBox from "../components/FilterBox";
 
 function page() {
-  return <div>page</div>;
+  return (
+    <div className="h-screen  m-6 mt-">
+      <div className="bg-white p-5">
+        <div className="flex justify-between items-center  ">
+          <div className="flex items-center gap-4">
+            <div className="font-semibold text-2xl">ABC PVT LTD</div>
+            <CustomBadge status={true} />
+          </div>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <EllipsisVertical />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem>Edit information</DropdownMenuItem>
+              <DropdownMenuItem>Pause</DropdownMenuItem>
+              <DropdownMenuItem>Change HR</DropdownMenuItem>
+              <DropdownMenuItem>Delete</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+
+        <div className="grid grid-cols-2 mt-4 gap-5 mb-5">
+          {" "}
+          <div className="flex bg-[#EFEEEB] gap-2 p-3 rounded-lg">
+            <div className="bg-gray-300  rounded-full p-3">
+              <Users />
+            </div>
+            <div>
+              <div>Total Hired Employees</div>
+              <div className="font-semibold">10212</div>
+            </div>
+          </div>
+          <div className="flex bg-[#EFEEEB] gap-2 p-3 rounded-lg">
+            <div className="bg-[#10B33133] rounded-full p-3">
+              <HandCoins />
+            </div>
+            <div>
+              <div>Total Commission</div>
+              <div className="font-semibold">$10212</div>
+            </div>
+          </div>
+        </div>
+        <Separator className="mt-2 mb-4" />
+        <div className="grid grid-cols-3 p-3">
+          <div>
+            <div className="text-[#4B5563]">Onboard Date</div>
+            <div className="font-semibold">12-06-2023</div>
+          </div>
+          <div>
+            <div className="text-[#4B5563]">Onboarded By</div>
+            <div className="font-semibold">Kamal Jain</div>
+          </div>
+          <div>
+            <div className="text-[#4B5563]">HR Allocated</div>
+            <div className="font-semibold">Jimmy Kim</div>
+          </div>
+        </div>
+        <div className="grid grid-cols-3 p-3">
+          <div>
+            <div className="text-[#4B5563]">Email</div>
+            <div className="font-semibold">abcd696969@gmail.com</div>
+          </div>
+          <div>
+            <div className="text-[#4B5563]">Phone Number</div>
+            <div className="font-semibold">9323333333333</div>
+          </div>
+          <div>
+            <div className="text-[#4B5563]">Address</div>
+            <div className="font-semibold">Dummy address</div>
+          </div>
+        </div>
+      </div>
+      <div className="font-semibold text-2xl m-2">Job List</div>
+      <div className="flex items-center gap-5">
+        <SearchBox />
+        <FilterBox />
+      </div>
+      <div className="bg-white p-3">
+        <JobTable />
+      </div>
+    </div>
+  );
 }
 
 export default page;
---
+++ src/app/admin/[companyId]/components/JobTable.tsx
@@ -0,0 +1,96 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { ExternalLink } from "lucide-react";
+
+import React from "react";
+
+function JobTable() {
+  return (
+    <Table>
+      <TableCaption>A list of your Jobs</TableCaption>
+      <TableHeader className="bg-[#D1D5DB] ">
+        <TableRow className="text-[#202020]">
+          <TableHead className="w-[250px] text-[#202020]">Job Name</TableHead>
+          <TableHead className="text-[#202020]">Job Type</TableHead>
+          <TableHead className="text-[#202020]">Employee Working</TableHead>
+          <TableHead className="text-[#202020]">Start Date</TableHead>
+          <TableHead className=""></TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        <TableRow>
+          <TableCell className="font-medium">Driver</TableCell>
+          <TableCell>Direct Hire</TableCell>
+          <TableCell>25</TableCell>
+          <TableCell className="">23-01-2023</TableCell>
+          <TableCell className="">
+            <ExternalLink />
+          </TableCell>
+        </TableRow>{" "}
+        <TableRow>
+          <TableCell className="font-medium">Driver</TableCell>
+          <TableCell>Direct Hire</TableCell>
+          <TableCell>25</TableCell>
+          <TableCell className="">23-01-2023</TableCell>
+          <TableCell className="">
+            <ExternalLink />
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className="font-medium">Driver</TableCell>
+          <TableCell>Direct Hire</TableCell>
+          <TableCell>25</TableCell>
+          <TableCell className="">23-01-2023</TableCell>
+          <TableCell className="">
+            <ExternalLink />
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className="font-medium">Driver</TableCell>
+          <TableCell>Direct Hire</TableCell>
+          <TableCell>25</TableCell>
+          <TableCell className="">23-01-2023</TableCell>
+          <TableCell className="">
+            <ExternalLink />
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className="font-medium">Driver</TableCell>
+          <TableCell>Direct Hire</TableCell>
+          <TableCell>25</TableCell>
+          <TableCell className="">23-01-2023</TableCell>
+          <TableCell className="">
+            <ExternalLink />
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className="font-medium">Driver</TableCell>
+          <TableCell>Direct Hire</TableCell>
+          <TableCell>25</TableCell>
+          <TableCell className="">23-01-2023</TableCell>
+          <TableCell className="">
+            <ExternalLink />
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell className="font-medium">Driver</TableCell>
+          <TableCell>Direct Hire</TableCell>
+          <TableCell>25</TableCell>
+          <TableCell className="">23-01-2023</TableCell>
+          <TableCell className="">
+            <ExternalLink />
+          </TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  );
+}
+
+export default JobTable;
